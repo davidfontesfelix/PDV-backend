dbb6f28b313f924ff2f2a5bacb77426874dcc30b
davidfontesfelix <davidfontes303@gmail.com>
2024-02-18 13:55:54 -0300

    criando rota de login

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { addDoc, arrayUnion, collection, doc, getDoc, getDocs, getFirestore, updateDoc } from 'firebase/firestore'
+import { addDoc, arrayRemove, arrayUnion, collection, doc, getDoc, getDocs, getFirestore, query, setDoc, updateDoc, where } from 'firebase/firestore'
 import { z } from 'zod'
 import '../../config/dotenv.config';
 
@@ -19,6 +19,7 @@ const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
 const CreateUserParamsSchema = z.object({
+  authenticated: z.boolean(),
   id: z.string(),
   name: z.string(),
   lastname: z.string(),
@@ -28,18 +29,39 @@ const CreateUserParamsSchema = z.object({
 
 type CreateUserParams = z.infer<typeof CreateUserParamsSchema>
 const createUser = async (user: CreateUserParams) => {
-  
   const usersCollection = doc(db, 'users', 'userDocs' )
   await updateDoc(usersCollection, {
     users: arrayUnion(user)
   })
 }
 
+type CredentialParams = z.infer<typeof CreateUserParamsSchema>
+
+const updateAuthenticationToTrue = async (userCredential: CredentialParams) => {
+  const userRef = doc(db, 'users', 'userDocs')
+  
+  await updateDoc(userRef, {
+    users: arrayUnion({ name: userCredential.name, id: userCredential.id, email: userCredential.email, lastname: userCredential.lastname, password: userCredential.password, authenticated: true })}
+  )
+  if (userCredential.authenticated === false) {
+    updateDoc(userRef, {
+      users: arrayRemove(userCredential)
+    })
+  }
+}
+
+// const loginUser = async () => {
+//   const usersCollection = collection(db, 'users')
+//   const data = await getDocs(usersCollection)
+//   const usersList = data.docs.map((doc) => doc.data())
+//   return usersList[0]
+// }
+
 const getUsers = async () => {
   const usersCollection = collection(db, 'users')
   const data = await getDocs(usersCollection)
-  const projectList = data.docs.map((doc) => doc.data())
-  return projectList[0].users
+  const usersList = data.docs.map((doc) => doc.data())
+  return usersList[0].users
 }
 
-export { createUser, getUsers }
+export { createUser, getUsers, updateAuthenticationToTrue }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,41 @@
 import express from 'express'
 import z, { ZodError } from 'zod'
-import { createUser, getUsers } from '../firebase/firebase'
+import { createUser, getUsers, updateAuthenticationToTrue } from '../firebase/firebase'
 const router = express.Router()
 import { v4 as uuidv4 } from 'uuid'
 
 
-
+ 
 router.get('/users', async (request, response) => {
   const data = await getUsers()
   return response.status(200).json(data) 
+})
+
+router.post('/login', async (request, response) => {
+  try {
+    const loginSchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(8)
+    })
+
+    const {email, password} = loginSchema.parse(request.body)
+
+    const data = await getUsers()
+    const userReturn = data.find((user: { email: string; password: string }) => user.email === email && user.password === password)
+
+    if(userReturn) {
+      await updateAuthenticationToTrue(userReturn)
+      return response.status(200).json({message: "Autorizado"})
+    } else {
+      return response.status(400).json({ error: "Usuario não encontrado"})
+    }
+  } catch (error) {
+    if (error instanceof ZodError) {
+      response.status(400).json({ error: 'Erro de validação dos dados' })
+    } else {
+      response.status(503).json({ error: 'O servidor não conseguiu receber os dados' })
+    }
+  }
 })
 
 router.post('/register', async (request, response) => {
@@ -20,18 +47,26 @@ router.post('/register', async (request, response) => {
       password: z.string().min(8)
     })
     const id = uuidv4()
+    const authenticated = false
 
     const { name, lastname, email, password } = createRegisterSchema.parse(request.body)
 
     const data = {
       id,
+      authenticated,
       name,
       lastname,
       email,
       password
     }
-    console.log(data)
-    await createUser(data)
+    const dataUsers = await getUsers()
+    const checkEmail = dataUsers.some((user: { email: string }) => user.email === email)
+
+    if(checkEmail) {
+      return response.status(400).json({error: 'Email já está em uso'})
+    } else {
+      createUser(data)
+    }
 
     response.status(201).json({ message: 'Usuario criado' })
   } catch (error) {
@@ -42,5 +77,6 @@ router.post('/register', async (request, response) => {
     }
   }
 })
+
 
 export { router }
